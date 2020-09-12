f639386c3148aac755570039c704e65cc001401d
rcouturedev <67654649+rcouturedev@users.noreply.github.com>
2020-09-12 13:40:13 -0400

    version one

--- app/src/main/java/com/example/simplecalc/MainActivity.java
+++ app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -1,32 +1,32 @@
 package com.example.simplecalc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     TextView result;
-    EditText number1,number2;
-    Button add,subtract,divide,multiply;
+    EditText number1, number2;
+    Button add, subtract, divide, multiply;
     float result_num;
-    Integer num1,num2;
+    Integer num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        result = (TextView)findViewById(R.id.result);
-        number1 = (EditText)findViewById(R.id.number1);
-        number2 = (EditText)findViewById(R.id.number2);
+        result = findViewById(R.id.result);
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
 
-        add = (Button)findViewById(R.id.Add);
-        subtract = (Button)findViewById(R.id.Subtract);
-        multiply = (Button)findViewById(R.id.Multiply);
-        divide = (Button)findViewById(R.id.Divide);
+        add = findViewById(R.id.Add);
+        subtract = findViewById(R.id.Subtract);
+        multiply = findViewById(R.id.Multiply);
+        divide = findViewById(R.id.Divide);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,28 +40,40 @@ public class MainActivity extends AppCompatActivity {
         subtract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num1 = Integer.parseInt(number1.getText().toString());
-                num2 = Integer.parseInt(number2.getText().toString());
-                result_num = num1 - num2;
-                result.setText(String.valueOf(result_num));
+                if (number1.getText().length() != 0 && number2.getText().length() != 0) {
+                    num1 = Integer.parseInt(number1.getText().toString());
+                    num2 = Integer.parseInt(number2.getText().toString());
+                    result_num = num1 - num2;
+                    result.setText(String.valueOf(result_num));
+                } else {
+                    result.setText("Invalid input");
+                }
             }
         });
         multiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num1 = Integer.parseInt(number1.getText().toString());
-                num2 = Integer.parseInt(number2.getText().toString());
-                result_num = num1 * num2;
-                result.setText(String.valueOf(result_num));
+                if (number1.getText().length() != 0 && number2.getText().length() != 0) {
+                    num1 = Integer.parseInt(number1.getText().toString());
+                    num2 = Integer.parseInt(number2.getText().toString());
+                    result_num = num1 * num2;
+                    result.setText(String.valueOf(result_num));
+                } else {
+                    result.setText("Invalid input");
+                }
             }
         });
         divide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                num1 = Integer.parseInt(number1.getText().toString());
-                num2 = Integer.parseInt(number2.getText().toString());
-                result_num = num1 / num2;
-                result.setText(String.valueOf(result_num));
+                if (number1.getText().length() != 0 && number2.getText().length() != 0) {
+                    num1 = Integer.parseInt(number1.getText().toString());
+                    num2 = Integer.parseInt(number2.getText().toString());
+                    result_num = num1 / num2;
+                    result.setText(String.valueOf(result_num));
+                } else {
+                    result.setText("Invalid input");
+                }
             }
         });
     }
